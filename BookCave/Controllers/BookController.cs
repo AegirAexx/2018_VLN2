9538eec9236419eeda4e5f8579eefd75ee95450d
Ice3vil <annamj17@ru.is>
2018-05-05 17:30:22 +0000

    Authentication

---
+++ BookCave/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookCave.Models;
+using BookCave.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookCave.Controllers
+{
+    [Authorize]
+    public class BookController : Controller
+    {
+        private BookService _bookService;
+
+        public BookController()
+        {
+            _bookService = new BookService();
+        }
+        public IActionResult Index()
+        {
+            var books = _bookService.GetAllBooks();
+            return View(books);
+        }
+    }
+}
