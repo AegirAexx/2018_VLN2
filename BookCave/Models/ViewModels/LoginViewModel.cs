421b4f60f71c5442d2c999929cbf585d60e8b528
ArnarArnar <arnara17@hr.is>
2018-05-07 19:05:17 +0000

    Minniháttar breyting
    Vitlaus villuskilaboð, lagað

--- BookCave/Models/ViewModels/LoginViewModel.cs
+++ BookCave/Models/ViewModels/LoginViewModel.cs
@@ -7,6 +7,7 @@ namespace BookCave.Models.ViewModels
         [Required(ErrorMessage = "Email address is required")]
         [EmailAddress]
         public string Email { get; set; }
+        
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
