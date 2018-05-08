90f36fd5383f784ad3add2d5ef516159d2bed558
ArnarArnar <arnara17@ru.is>
2018-05-08 19:52:11 +0000

    UserService og breytingar á  LoginPartial

--- BookCave/Controllers/HomeController.cs
+++ BookCave/Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -35,7 +34,7 @@ namespace BookCave.Controllers
             return View(books);
         }
 
-        public IActionResult Getorders()
+        public IActionResult GetOrders()
         {
             var orders = _orderService.GetAllOrders();
             return View(orders);
@@ -68,76 +67,3 @@ namespace BookCave.Controllers
 
     }
 }
-
-=======
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using BookCave.Models;
-using BookCave.Services;
-using BookCave.Models.InputModels;
-using BookCave.Data.EntityModels;
-
-namespace BookCave.Controllers
-{
-    public class HomeController : Controller
-    {
-        private BookService _bookService;
-
-        private OrderService _orderService;
-
-        public HomeController()
-        {
-            _bookService = new BookService();
-
-            _orderService = new OrderService();
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult SeeBooks()
-        {
-            var books = _bookService.GetAllBooks();
-            return View(books);
-        }
-
-        public IActionResult Getorders()
-        {
-            var orders = _orderService.GetAllOrders();
-            return View(orders);
-        }
-
-        /*   //Ég á eftir að breyta þessu í order - Dagur
-        [HttpPost]
-        public IActionResult AddUser(UserInputModel inputUser)
-        {
-            if(ModelState.IsValid)
-            {
-                var newUser = new User
-                {
-                    UserName = inputUser.UserName,
-                    FullName = inputUser.FullName,
-                    Email = inputUser.Email,
-                    Gender = inputUser.Gender,
-                    Age = inputUser.Age,
-                    FavoriteBook = inputUser.FavoriteBook,
-                    IsActive = 1,
-                    JobTitle = "User",
-                    Image = ""
-                };
-
-                _userService.AddUser(newUser);
-                return RedirectToAction("Index");
-            }
-            return View();
-        }*/
-
-    }
-}
-
->>>>>>> 146e6f0540903de7b768c01eb5730ab64108965f
