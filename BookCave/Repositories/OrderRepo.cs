4430e5863061d5790c236093184e2f3a9da4e93f
ArnarArnar <arnara17@ru.is>
2018-05-08 17:22:05 +0000

    Breytingar

---
+++ BookCave/Repositories/OrderRepo.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using BookCave.Data;
+using BookCave.Models.ViewModels;
+
+namespace BookCave.Repositories
+{
+    public class OrderRepo
+    {
+        private DataContext _db;
+
+        public OrderRepo()
+        {
+            _db = new DataContext();
+        }
+
+       public List<OrderListViewModel> GetAllOrders()
+       {
+           var orders = (from o in _db.Orders
+                        select new OrderListViewModel
+                        {
+                           Id = o.Id,
+                           CustomerId = o.CustomerId,
+                           UserName = o.UserName,
+                           TotalPrice = o.TotalPrice,
+                           OrderStatus = o.OrderStatus,
+                           OrderItems = (from oi in _db.OrderItems
+                                        where oi.OrderId == o.Id
+                                        select new OrderItemViewModel
+                                        {
+                                            BookId = oi.BookId,
+                                            Price = oi.Price,
+                                            Book = (from b in _db.Books
+                                                    where b.Id == oi.BookId
+                                                    select new BookListViewModel
+                                                    {
+                                                        Title = b.Title,
+                                                        Id = b.Id
+                                                    }).ToList()
+                                        }).ToList()
+                        }).ToList();
+           return orders;
+       }
+
+    }
+}
