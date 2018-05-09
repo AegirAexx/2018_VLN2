46e02d22ec30d561345fbd745bc107392bfbafeb
ArnarArnar <arnara17@ru.is>
2018-05-09 21:48:03 +0000

    Book details page

--- BookCave/wwwroot/js/site.js
+++ BookCave/wwwroot/js/site.js
@@ -39,4 +39,36 @@ $(document).ready(function () {
     });
     //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
 
+    //Hér fyrir neðan er js fyrir details síðu
+    $("ul.menu-items > li").on("click",function(){
+        $("ul.menu-items > li").removeClass("active");
+        $(this).addClass("active");
+    })
+
+    $(".attr,.attr2").on("click",function(){
+        var clase = $(this).attr("class");
+
+        $("." + clase).removeClass("active");
+        $(this).addClass("active");
+    })
+
+    //-- Click on QUANTITY
+    $(".btn-minus").on("click",function(){
+        var now = $(".section > div > input").val();
+        if ($.isNumeric(now)){
+            if (parseInt(now) -1 > 0){ now--;}
+            $(".section > div > input").val(now);
+        }else{
+            $(".section > div > input").val("1");
+        }
+    })            
+    $(".btn-plus").on("click",function(){
+        var now = $(".section > div > input").val();
+        if ($.isNumeric(now)){
+            $(".section > div > input").val(parseInt(now)+1);
+        }else{
+            $(".section > div > input").val("1");
+        }
+    }) 
+
 });
